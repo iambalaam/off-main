21405630089c5019f8011d6ba1f883f0f82dc64a
Guy Balaam <guy@Pimlocs-MacBook-Pro.local>
2025-03-14 08:46:46 +0000

    Opening and closing server and browser

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,14 +1,20 @@
 import { expect } from "@std/expect";
 import { afterAll, beforeAll, describe, it } from "@std/testing/bdd";
 import { Browser, launch } from "astral";
+import { StaticServer } from "./StaticServer.ts";
+
+let server: StaticServer;
 
 describe("browser tests", () => {
   let browser: Browser;
   beforeAll(async () => {
     browser = await launch();
+    server = new StaticServer();
+    server.listen(3000);
   });
   afterAll(async () => {
     await browser?.close();
+    server.close();
   });
   it("can find root element", async () => {
     const page = await browser.newPage("http://localhost:3000");
