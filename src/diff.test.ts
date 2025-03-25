259f3670c38f2f230b1afc0825966d88be7ffcdd
Guy Balaam <guy.balaam@gmail.com>
2025-03-25 18:06:29 +0000

    Adding replacement

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -3,16 +3,16 @@ import { diff, id } from "./diff.ts";
 
 Deno.test("creates a div", () => {
   id.next = 0;
-  expect(diff({ tag: "div" })).toEqual([[0, "createElement", "div"]]);
+  expect(diff({ tag: "div" })).toEqual([0, [[0, "createElement", "div"]]]);
 });
 
 Deno.test("adds a class", () => {
   id.next = 0;
   expect(diff({ tag: "div", props: { class: "test-class" } }))
-    .toEqual([
+    .toEqual([0, [
       [0, "createElement", "div"],
       [0, "setAttribute", "class", "test-class"],
-    ]);
+    ]]);
 });
 
 Deno.test("adds text content", () => {
@@ -21,11 +21,11 @@ Deno.test("adds text content", () => {
     tag: "div",
     children: ["Hello, world!"],
   }))
-    .toEqual([
+    .toEqual([0, [
       [0, "createElement", "div"],
-      [1, "value", "Hello, world!"],
+      [1, "text", "Hello, world!"],
       [0, "replaceChildren", [1]],
-    ]);
+    ]]);
 });
 
 Deno.test("adds children", () => {
@@ -37,14 +37,72 @@ Deno.test("adds children", () => {
       { tag: "li", children: ["2"] },
     ],
   }))
-    .toEqual([
+    .toEqual([0, [
       [0, "createElement", "ul"],
       [1, "createElement", "li"],
-      [2, "value", "1"],
+      [2, "text", "1"],
       [1, "replaceChildren", [2]],
       [3, "createElement", "li"],
-      [4, "value", "2"],
+      [4, "text", "2"],
       [3, "replaceChildren", [4]],
       [0, "replaceChildren", [1, 3]],
-    ]);
+    ]]);
+});
+
+Deno.test("changes text content", () => {
+  id.next = 1;
+  expect(
+    diff(
+      { tag: "span", children: ["After"] },
+      { tag: "span", id: 0, children: ["Before"] },
+    ),
+  ).toEqual([0, [
+    [1, "text", "After"],
+    [0, "replaceChildren", [1]],
+  ]]);
+});
+
+Deno.test("changes props", () => {
+  id.next = 1;
+  expect(
+    diff(
+      {
+        tag: "span",
+        props: { class: "after-class" },
+        children: ["Lorem ipsum"],
+      },
+      {
+        id: 0,
+        tag: "span",
+        props: { class: "before-class" },
+        children: ["Lorem ipsum"],
+      },
+    ),
+  ).toEqual([0, [
+    [0, "setAttribute", "class", "after-class"],
+    [1, "text", "Lorem ipsum"],
+    [0, "replaceChildren", [1]],
+  ]]);
+});
+
+Deno.test("Removes child", () => {
+  id.next = 2;
+  expect(
+    diff(
+      {
+        tag: "div",
+        children: [{ tag: "button" }],
+      },
+      {
+        id: 0,
+        tag: "div",
+        children: [{
+          id: 1,
+          tag: "button",
+        }, "Click Me!"],
+      },
+    ),
+  ).toEqual([0, [
+    [0, "replaceChildren", []],
+  ]]);
 });
--- src/diff.ts
+++ src/diff.ts
@@ -8,22 +8,29 @@ export type VDOMNode =
   | string;
 
 export type Instruction =
-  | [number, "value", string]
+  | [number, "text", string]
+  | [number, "function", string]
   | [number, "createElement", string]
   | [number, "setAttribute", string, (string | number)]
+  | [number, "removeAttribute", string]
   | [number, "replaceChildren", (string | number)[]];
 
 export const id = { next: 0 };
 export const fns = [];
 
-export function diff(to?: VDOMNode, from?: VDOMNode): Instruction[] {
-  if (to === undefined) return [];
+export function diff(
+  to: VDOMNode,
+  from?: VDOMNode,
+): [number, Instruction[]] | undefined {
+  if (typeof to === "string" || typeof to === "number") {
+    const i = id.next++;
+    return [i, [[i, "text", `${to}`]]];
+  }
+
   const instructions: Instruction[] = [];
 
-  if (from === undefined) {
-    if (typeof to === "string") return [[id.next++, "value", to]];
-    if (typeof to === "number") return [[id.next++, "value", `${to}`]];
-
+  if (from === undefined || typeof from === "string") {
+    // Create new tag
     to.id = id.next++;
     instructions.push([to.id, "createElement", to.tag]);
 
@@ -43,8 +50,7 @@ export function diff(to?: VDOMNode, from?: VDOMNode): Instruction[] {
       const childIds: number[] = [];
 
       for (const child of to.children) {
-        const childInstructions = diff(child);
-        const childId = childInstructions?.[0]?.[0];
+        const [childId, childInstructions] = diff(child) || [];
         if (!childId) {
           throw new Error(
             `Could not find child id in instructions: ${
@@ -53,10 +59,66 @@ export function diff(to?: VDOMNode, from?: VDOMNode): Instruction[] {
           );
         }
         childIds.push(childId);
-        instructions.push(...childInstructions);
+        instructions.push(...childInstructions || []);
       }
       instructions.push([to.id, "replaceChildren", childIds]);
     }
+    return [to.id, instructions];
   }
-  return instructions;
+
+  if (to.tag === from.tag) {
+    // Reuse component
+    if (from.id === undefined) {
+      throw new Error(`No id on node: ${JSON.stringify(from)}`);
+    }
+
+    to.id = from.id;
+    if (to.props) {
+      for (const [prop, value] of Object.entries(to.props)) {
+        if (typeof prop === "function") {
+          const fnId = id.next++;
+          fns[fnId] = prop;
+          instructions.push([to.id, "setAttribute", prop, fnId]);
+        } else {
+          instructions.push([to.id, "setAttribute", prop, `${value}`]);
+        }
+      }
+    }
+    if (from.props) {
+      for (const prop of Object.keys(from.props)) {
+        if (!to.props || !(prop in to.props)) {
+          instructions.push([to.id, "removeAttribute", prop]);
+        }
+      }
+    }
+
+    if (to.children || from.children) {
+      const toChildren = to.children || [];
+      const fromChildren = from.children || [];
+
+      const childIds: number[] = [];
+      const childInstructions: Instruction[] = [];
+
+      for (let i = 0; i < toChildren.length; i++) {
+        const toChild = toChildren[i];
+        const fromChild = fromChildren[i];
+        const [cId, cInst] = diff(toChild!, fromChild) || [];
+        if (cInst?.length) {
+          if (!cId) {
+            throw new Error(
+              `Could not find child id in instructions: ${
+                JSON.stringify(cInst)
+              }`,
+            );
+          }
+          childInstructions.push(...cInst);
+          childIds.push(cId);
+        }
+      }
+
+      instructions.push(...childInstructions);
+      instructions.push([to.id, "replaceChildren", childIds]);
+      return [to.id!, instructions];
+    }
+  }
 }
